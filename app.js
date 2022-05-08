787f743396d1e474b72cd6efdf6562566708925a
kaderabdul <kaderabdul7980@gmail.com>
2022-05-08 23:09:24 +0600

    connected to the database

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello from book store!")
+
+// JIkM1xWYqyu8vdyN
+// admin
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://admin:JIkM1xWYqyu8vdyN@cluster0.8mxfw.mongodb.net/Book-Store?retryWrites=true&w=majority"
+).then(()=>console.log("connected to the database"));
+
