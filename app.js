75ce87c9dbc56b1fedba7eba5ca8947cf4f03af0
kaderabdul <kaderabdul7980@gmail.com>
2022-05-09 00:06:38 +0600

    create a basic middleware

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // JIkM1xWYqyu8vdyN
 // admin
 
@@ -6,7 +5,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-mongoose.connect(
-  "mongodb+srv://admin:JIkM1xWYqyu8vdyN@cluster0.8mxfw.mongodb.net/Book-Store?retryWrites=true&w=majority"
-).then(()=>console.log("connected to the database"));
+// Middlewares
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Book Store Homepage</h1>')
+})
 
+mongoose
+  .connect(
+    "mongodb+srv://admin:JIkM1xWYqyu8vdyN@cluster0.8mxfw.mongodb.net/Book-Store?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connected to the database"))
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
